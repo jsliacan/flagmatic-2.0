face2729cf80359bd558af00e928d479bd48f652
Jakub Sliacan <jakub.sliacan@gmail.com>
2014-12-09 23:30:49 +0000

    updated graph_flag.pyx and hypergraph_flag.pyx to run with Sage 6.4.1

--- pkg/flagmatic/hypergraph_flag.pyx
+++ pkg/flagmatic/hypergraph_flag.pyx
@@ -599,7 +599,8 @@ cdef class HypergraphFlag (Flag):
 		for fg in flags:
 			mfgs = str(fg)
 			for perm in Permutations(range(1, s + 1)):
-				permplus = perm + range(s + 1, fg.n + 1)
+				#permplus = perm + range(s + 1, fg.n + 1)
+				permplus = list(perm) + range(s + 1, fg.n + 1)
 				ntg = tg.__copy__()
 				ntg.relabel(perm)
 				nfg = fg.__copy__()
